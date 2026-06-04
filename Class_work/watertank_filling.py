8ea035f819f7e98f7062228344f70bae411727be
Avika115 <srivastava.avika2006@gmail.com>
2026-06-04 12:07:05 +0530

    Add files via upload

---
+++ Class_work/watertank_filling.py
@@ -0,0 +1,18 @@
+#program of water tank filling
+#litter per minute
+rate = 10          
+water = 0
+capacity = 100
+
+while water < capacity:
+    water += rate
+#-------------------------------------
+#validation
+    if water > capacity:      
+        water = capacity
+
+    print("Water in tank:", water, "liters")
+#-------------------------------------------    
+
+print("Tank is full.")
+
