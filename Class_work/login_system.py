7a8a73f38722250ac430fdef75f85751e2219d5d
Avika115 <srivastava.avika2006@gmail.com>
2026-06-05 23:06:13 +0530

    Delete Class_work/login_system.py

--- Class_work/login_system.py
+++
@@ -1,9 +0,0 @@
-#program for login system
-password="admin123"
-user_password=input("Enter your password: ")
-
-while(user_password!=password):
-    print("Incorrect password. Try again.")
-    user_password=input("Enter your password: ")
-
-print("Login successful")
