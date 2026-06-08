e451d3d96a1c10cc3052ce058c34f81858353cfd
Avika115 <srivastava.avika2006@gmail.com>
2026-06-08 15:45:11 +0530

    Delete Class_work/dictionary_8thjune directory

--- Class_work/dictionary_8thjune/sentence.py
+++
@@ -1,8 +0,0 @@
-#WAP to input a setence from user and count the number of special characters present in the sentence 
-
-sentence = input("enter a sentence")
-count = 0
-for ch in sentence:
-    if ch.isalnum()==False:
-        count+1
-print("the number of special characters", count)
